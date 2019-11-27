09e80fbfe34def2b945c7158f77073b2e580c692
ivl <igor.vlasiuk@gmail.com>
2019-11-27 09:33:52 +0200

    TestAssignment: First Approach

---
+++ TestAssignment/TestAssignment/Image Library/CoolImageLibrary.swift
@@ -0,0 +1,51 @@
+//
+//  CoolImageLibrary.swift
+//  TestAssignment
+//
+//  Created by Ihor Vlasyuk on 11/27/19.
+//  Copyright © 2019 Ihor Vlasyuk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// DON'T EXPECT GRAND DESIGNS
+// KEEP IT SIMPLE STUPID
+
+public class CoolImageLibrary: NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
+    
+ private  var delegate: CoolImageLibraryDownloadProgressDelegate?
+
+ public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+    
+    DispatchQueue.main.async {
+        self.delegate?.providePercentage(progress: Float(totalBytesWritten) / Float(totalBytesExpectedToWrite))
+    }
+ }
+    
+ public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+            if let data = try? Data(contentsOf: location), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.delegate?.imageDownloaded(image: image)
+                }
+            } else {
+               self.delegate?.downloadingErorr()
+            }
+    }
+    
+    
+    public func downloadImage(imgUrl: String) {
+        let url = URL(string: imgUrl)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
+        session.downloadTask(with: url!).resume()
+    }
+}
+    
+public protocol CoolImageLibraryDownloadProgressDelegate {
+    func imageDownloaded(image: UIImage)
+    func downloadingErorr()
+    func providePercentage(progress: Float)
+}
+
+
--- TestAssignment/TestAssignment/ViewController.swift
+++ TestAssignment/TestAssignment/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDelegate, URLSessionDownloadDelegate {
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        <#code#>
+    }
+    
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    private let byteFormatter: ByteCountFormatter = {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = [.useKB, .useMB]
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func downloadImage(sender : AnyObject) {
+        // A 10MB image from NASA
+        let url = URL(string: "https://photojournal.jpl.nasa.gov/jpeg/PIA08506.jpg")!
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
+        
+        // Don't specify a completion handler here or the delegate won't be called
+        session.downloadTask(with: url).resume()
     }
 
 
